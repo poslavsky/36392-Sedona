c6e5aa0733afd395ff1c721027e5c248819485ba
Alexandr Priadko <alexandrpriad@gmail.com>
2015-03-25 18:02:18 +0200

    Js+css first

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -1,5 +1,33 @@
 (function() {
 
-  
+  var plus = document.querySelector(".plus");
+  var minus = document.querySelector(".minus");
+  var counter = document.querySelector(".counter-input").value;
+
+
+  plus.addEventListener("click", function () {
+    event.preventDefault();
+
+    var i = 0;
+    var counter = i;
+
+    return function () {
+      i++;
+      return i;
+    };
+
+
+
+  });
+
+/*var xhr = new XMLHttpRequest();
+xhr.open("get", "hello.txt");
+xhr.addEventListener("readystatechange", function() {
+if (xhr.readyState == 4) {
+console.log(xhr.responseText);
+}
+});
+
+xhr.send();*/
 
 })();
